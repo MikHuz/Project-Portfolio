aab9483e5928f95a40af0c55a85595ca61a616d2
MikHuz <michaelhuziy@gmail.com>
2025-03-28 19:37:32 -0700

    Pushing Team Project

---
+++ Photo & Image Search/README.md
@@ -0,0 +1,9 @@
+This project is an image and video search, combined with AI image generation as well. 
+It is utilizing the Pixabay API to return images, Pexels API to retrieve videos based on keywords, and RapidAPI to generate AI content.
+Two options exist, one to show a grid view of the images/videos, and one to get a slideshow of them
+You may zoom in on an image by hovering over it, and additonally you may click the image to open it in a new window to better view and download.
+Zooming in on videos naturally applies with the fullscreen function provided by html.
+
+To Run: Install the following python modules: requests, flask, bootstrap-flask. Once done, cd into the project, and run the project with "flask --app app --debug run"
+
+Future Work: Stylizing the project a bit more to appear more attractive, adding options for a user to manage how much results they wish to display. 
---
+++ Photo & Image Search/images_api.py
@@ -0,0 +1,50 @@
+import requests
+# from flask import Flask, render_template, request
+# from flask_bootstrap import Bootstrap5
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, SubmitField
+# from wtforms.validators import DataRequired
+# from datetime import datetime
+
+# Worked on by Michael
+# Function to call image api and return array of jsons and array of strings
+def get_photos(search_term, pixa_bay_key):
+    photos = []
+    unique_tags = []
+    photo_r= requests.get(f"https://pixabay.com/api/?key={pixa_bay_key}&q={search_term}&image_type=photo")
+
+    if photo_r.status_code == 200:
+        data = photo_r.json()
+        # Check if data is not empty, and pull relevant information out
+        if (data['total'] != 0):
+            #pprint.pprint(data["hits"])  # Debug print statement to check fetched data
+            for photo in data['hits']:
+                #print(f"Type: {photo["type"]}")
+                photos.append(photo['webformatURL'])
+                tags = photo['tags'].split(',')
+                for tag in tags:
+                    tag = tag.strip()
+                    # Pull out unique tags
+                    if tag not in unique_tags:
+                        unique_tags.append(tag)
+                #pprint.pprint(images)  # Debug print statement to check fetched images
+                #print(unique_tags)  # Debug print statement to check fetched unique tags
+    return photos,unique_tags
+
+# Function to call image api for illustrations and return array of jsons
+
+def get_illustrations(search_term, pixa_bay_key):
+    illustrations = []
+    illustration_r = requests.get(f"https://pixabay.com/api/?key={pixa_bay_key}&q={search_term}&image_type=illustration")
+    # vector_r =  requests.get(f"https://pixabay.com/api/?key={pixa_bay_key}&q={search_term}&image_type=vector") 
+    #Was thinking of combing vecotor with illustrations as one result, or make another category
+    #for users who want svg images specfically to view/download
+
+    if illustration_r.status_code == 200:
+        data = illustration_r.json()
+        # Check if data is not empty and pull relevant information out
+        if (data['total'] != 0):
+            for illustration in data['hits']:
+                #print(f"Type: {illustration["type"]}")
+                illustrations.append(illustration['webformatURL'])
+    return illustrations
